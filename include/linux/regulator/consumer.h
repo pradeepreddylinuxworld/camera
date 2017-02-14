5e86bc679e7fc79a68bc7c1e72042ddfa5d98b1a
Pradeep M <pradeep@inforcecomputing.com>
2017-02-14 20:38:39 +0530

    include: regulator-set-load added for compile
    Change-Id: Ie9ee0c556e002be208606363201c5852a0f1f75c

--- include/linux/regulator/consumer.h
+++ include/linux/regulator/consumer.h
@@ -249,6 +249,9 @@ int regulator_get_current_limit(struct regulator *regulator);
 
 int regulator_set_mode(struct regulator *regulator, unsigned int mode);
 unsigned int regulator_get_mode(struct regulator *regulator);
+
+int regulator_set_load(struct regulator *regulator, int load_uA);
+
 int regulator_set_optimum_mode(struct regulator *regulator, int load_uA);
 
 int regulator_allow_bypass(struct regulator *regulator, bool allow);
